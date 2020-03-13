77880c370778f12778c320fb8193a32d604e0ebb
hichamcheniti <hicham_c90@hotmail.com>
2020-03-12 20:41:22 -0400

    (ZHC)[dev]: huffman correction

--- tp2/jpegCoding.py
+++ tp2/jpegCoding.py
@@ -2,9 +2,12 @@ import cv2
 import sys
 import numpy as np
 from utils import *
+from huffman import HuffmanCoding
+
 
 image = cv2.imread("Fichiers_TP2-2/data/kodim01.png", cv2.IMREAD_COLOR)
 
+print(sys.getsizeof(image))
 
 cv2.imshow("Normale", image)
 initialeSize = sys.getsizeof(image)
@@ -16,14 +19,19 @@ cv2.imshow("YCC", image)
 
 # Etape 2 : echantillionage de l'image 4:2:0:
 # source : https://medium.com/@sddkal/chroma-subsampling-in-numpy-47bf2bb5af83
-# image[1::2] = image[::2, :]
-# image[:, 1::2] = image[:, ::2]
+image[1::2] = image[::2, :]
+image[:, 1::2] = image[:, ::2]
 
-chrominance(image)
-cv2.imshow("Subsampling", image)
+img2 = image
+chrominance(img2)
+
+cv2.imshow("Subsampling- 1", image)
+cv2.imshow("Subsampling- 2", img2)
+
 
 # Etape 3: Decoupage en blocs
 blocs = blocks(image)
+
 
 #Etape3 : Dct sur chaque block:
 for j in range(blocs.shape[1]):
@@ -38,9 +46,15 @@ for j in range(blocs.shape[1]):
 for j in range(blocs.shape[1]):
     for i in range(blocs.shape[0]):
         blocs[i,j] = zigzag(blocs[i,j])
+        blocs_string = ''.join([str(elem) for elem in blocs[i,j]])
+        compressed_bloc = HuffmanCoding.compress(blocs_string)
+        ##print(compressed_bloc)
+
 #Etape 6 huffman and RLE
-
-
+# list = list(blocs)
+# compressed_bloc = HuffmanCoding.compress(list)
+# print(compressed_bloc)
+print(sys.getsizeof(blocs))
 #cv2.imshow("Normal", image)
 #cv2.imshow("YCC", YCCimage)
 cv2.waitKey()
